fd9fced7fd85e2e1cd07ff103570e16cb8fa6e1c
Илья <64031009+Ility-47@users.noreply.github.com>
2022-12-04 21:47:20 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const header = document.getElementById('header');
+const about = document.getElementById('about');
+const footer = document.getElementById('footer');
+
+let count = 0;
+
+window.addEventListener("wheel", (e) =>{
+    
+    let delta = e.deltaY;
+    count += delta;
+    
+    if(window.getComputedStyle(header).opacity == 1 && count > 200){
+        about.style.opacity = 1;
+        about.style.top = 0;
+        header.style.opacity = 0;
+        header.style.top = -90+'vh';
+        count = 0
+    }else if(window.getComputedStyle(about).opacity == 1 && count > 200){
+        footer.style.opacity = 1;
+        footer.style.bottom = 0;
+        count = 0
+    }else if(window.getComputedStyle(footer).opacity == 1 && count < -100){
+        footer.style.opacity = 0;
+        footer.style.bottom = -100 +'px';
+        count = 0
+    }else if(window.getComputedStyle(about).opacity == 1 && count < -300){
+        about.style.opacity = 0;
+        about.style.top = 100 + 'px';
+        header.style.opacity = 1;
+        header.style.top = 0;
+        count = 0
+    }
+
+})
