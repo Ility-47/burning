b6b9b0cb97d73fa36543fe0aa46c7975e0ab3a2b
Илья <64031009+Ility-47@users.noreply.github.com>
2022-12-05 18:21:38 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,30 @@ window.addEventListener("wheel", (e) =>{
     }
 
 })
+
+window.addEventListener("touchstart", (event) =>{
+
+    let touch = event.changedTouches[0].pageY
+    if(window.getComputedStyle(header).opacity == 1 && touch > 680){
+        about.style.opacity = 1;
+        about.style.top = 0;
+        header.style.opacity = 0;
+        header.style.top = -90+'vh';
+         
+    }else if(window.getComputedStyle(about).opacity == 1 && touch > 680){
+        footer.style.opacity = 1;
+        footer.style.bottom = 0;
+        
+    }else if(window.getComputedStyle(footer).opacity == 1 && touch < 340){
+        footer.style.opacity = 0;
+        footer.style.bottom = -100 +'px';
+        
+    }else if(window.getComputedStyle(about).opacity == 1 && touch < 340){
+        about.style.opacity = 0;
+        about.style.top = 100 + 'px';
+        header.style.opacity = 1;
+        header.style.top = 0;
+        
+    }
+       
+})
